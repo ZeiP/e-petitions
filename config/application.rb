56112115f5e6be008094c6c56c40722fe7edcc2e
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2019-09-24 17:32:11 +0300

    More localisation and a bit customisation for the Scouts and Guides of Finland

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Epets
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :'en-GB'
+    config.i18n.default_locale = :'fi-FI'
 
     # Use SQL for the schema format
     config.active_record.schema_format = :sql
