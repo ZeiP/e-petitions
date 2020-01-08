0b02952a3b5f9e07747683b1d6224264048d4955
Petteri Saari <petteri.saari@trineria.fi>
2020-01-08 20:39:46 +0200

    marking work-to-do

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
-require File.expand_path('../boot', __FILE__)
-require File.expand_path('../../lib/cloud_front_remote_ip', __FILE__)
+require File.expand_path("../boot", __FILE__)
+require File.expand_path("../../lib/cloud_front_remote_ip", __FILE__)
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,19 +15,20 @@ module Epets
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'London'
+    config.time_zone = "London"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    #config.i18n.default_locale = :'en-GB'
     config.i18n.default_locale = :'fi-FI'
 
     # Use SQL for the schema format
     config.active_record.schema_format = :sql
 
     # Configure the cache store
-    config.cache_store = :atomic_dalli_store, ENV.fetch('MEMCACHED_HOST'), {
-      namespace: 'epets', expires_in: 1.day, compress: true,
-      pool_size: ENV.fetch('WEB_CONCURRENCY_MAX_THREADS') { 32 }.to_i
+    config.cache_store = :atomic_dalli_store, ENV.fetch("MEMCACHED_HOST"), {
+      namespace: "epets", expires_in: 1.day, compress: true,
+      pool_size: ENV.fetch("WEB_CONCURRENCY_MAX_THREADS") { 32 }.to_i,
     }
 
     # Configure Active Job queue adapter
@@ -38,14 +39,14 @@ module Epets
 
     # Add additional exceptions to the rescue responses
     config.action_dispatch.rescue_responses.merge!(
-      'Site::ServiceUnavailable' => :service_unavailable,
-      'BulkVerification::InvalidBulkRequest' => :bad_request
+      "Site::ServiceUnavailable" => :service_unavailable,
+      "BulkVerification::InvalidBulkRequest" => :bad_request,
     )
 
-    config.action_dispatch.default_headers.merge!('X-UA-Compatible' => 'IE=edge')
+    config.action_dispatch.default_headers.merge!("X-UA-Compatible" => "IE=edge")
 
     # Needed as Rails does not add app/jobs/concerns to the load path
-    config.paths.add 'app/jobs/concerns', eager_load: true
+    config.paths.add "app/jobs/concerns", eager_load: true
 
     # Replace ActionDispatch::RemoteIp with our custom middleware
     # to remove the CloudFront ip address from X-Forwarded-For
