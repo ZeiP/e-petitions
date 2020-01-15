a6008ef475e96c42230c5876ad12d262f2db404f
Petteri Saari <petteri.saari@trineria.fi>
2020-01-15 16:09:56 +0200

    datetime localization

--- app/helpers/date_time_helper.rb
+++ app/helpers/date_time_helper.rb
@@ -2,13 +2,12 @@ module DateTimeHelper
   WAITING_FOR_KEYS = Hash.new(:other).merge(0 => :zero, 1 => :one)
   TO_BE_DEBATED_KEYS = Hash.new(:other).merge(0 => :today, 1 => :tomorrow)
 
-  #TODO: LOCALIZE
   def short_date_format(date_time)
-    date_time && date_time.strftime("%-d %B %Y")
+    date_time && date_time.strftime(t "date.formats.short")
   end
 
   def short_date_time_format(date_time)
-    date_time && date_time.strftime("%H:%M%P on %-d %B %Y")
+    date_time && date_time.strftime(t "datetime.formats.short")
   end
 
   def date_time_format(date_time, seconds: false)
@@ -39,8 +38,8 @@ module DateTimeHelper
     return unless date.present?
 
     scope = :"petitions.waiting_for_in_words"
-    days  = ((now.end_of_day - date.end_of_day) / 86400.0).round
-    key   = WAITING_FOR_KEYS[days]
+    days = ((now.end_of_day - date.end_of_day) / 86400.0).round
+    key = WAITING_FOR_KEYS[days]
 
     t(key, scope: scope, formatted_count: number_with_delimiter(days))
   end
@@ -57,8 +56,8 @@ module DateTimeHelper
 
   def scheduled_for_debate_in_words(date, today = Date.current)
     scope = :"petitions.scheduled_for_debate_in_words"
-    days  = (date - today).to_i
-    key   = TO_BE_DEBATED_KEYS[days]
+    days = (date - today).to_i
+    key = TO_BE_DEBATED_KEYS[days]
 
     t(key, scope: scope, formatted_date: short_date_format(date))
   end
