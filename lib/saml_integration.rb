df480912ba898b7d9711025efe397eb89680c022
Leif Setälä <leif.setala@trineria.fi>
2020-01-16 09:27:09 +0200

    adds idp_cert to saml configuration

--- lib/saml_integration.rb
+++ lib/saml_integration.rb
@@ -22,7 +22,8 @@ module SamlIntegration
     @saml_configuration = @saml_configuration || OneLogin::RubySaml::Settings.new({
       assertion_consumer_service_url: ENV['SAML_ACS_URL'],
       assertion_consumer_logout_service_url: ENV['SAML_LOGOUT_URL'],
-      idp_sso_target_url: ENV['SAML_IDP_SSO_TARGET_URL']
+      idp_sso_target_url: ENV['SAML_IDP_SSO_TARGET_URL'],
+      idp_cert: ENV['SAML_IDP_CERT']
     })
     return @saml_configuration
   end
