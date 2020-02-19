4941e17e5853d25b1a9c233140790eefb9dedf09
Leif Setälä <leif.setala@trineria.fi>
2020-02-19 11:42:33 +0200

    use mailtrap for testing emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,15 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Tell Action Mailer not to deliver emails to the real world.
-  config.action_mailer.delivery_method = :file
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch('SMTP_HOSTNAME'),
+    port: ENV.fetch('SMTP_PORT'),
+    user_name: ENV.fetch('SMTP_USERNAME'),
+    password: ENV.fetch('SMTP_PASSWORD'),
+    authentication: :login,
+    enable_starttls_auto: true
+  }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
