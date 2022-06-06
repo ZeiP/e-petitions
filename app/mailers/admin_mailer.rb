7e76cf82f8b0411ee5f06e9ea3f65c5aa7a6e8ac
phuonglaitinen <phuong.laitinen@trineria.fi>
2022-06-06 15:07:19 +0300

    created view for notification email, added sending notification to admin mailer, added temporary translation

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -5,4 +5,25 @@ class AdminMailer < ActionMailer::Base
     @petitions = petitions
     mail(subject: "Petitions alert", to: admin_users.map(&:email))
   end
+
+  def notify_admin_of_petition_created(new_petition)
+    @new_petition = new_petition
+    subject = subject_for(:petition_created)
+    recipient_email = "partiolaisaloite@partio.fi"
+    mail to: recipient_email , subject: subject
+  end
+
+  def subject_for(key, options = {})
+    I18n.t key, i18n_options.merge(options)
+  end
+
+  def i18n_options
+    {}.tap do |options|
+      options[:scope] = :"mail.admin"
+
+      if defined?(@subject)
+        options[:subject] = @subject
+      end
+    end
+  end
 end
